2a650784dcb975b4244e8b85cd27fc8cc2ce0b05
Jeferson Braun <jefersonbbraun@gmail.com>
2013-08-27 01:55:19 -0300

    single.php
    funcionando local, ainda sem muita formatação

--- single.php
+++ single.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-		<h1><?php the_title(); ?></h1>
-		<span>like tweet</span>
-		<?php the_content(); ?>
+<div class="row paddingT36">
+    <div class="eightcol">
+            <h2 style="margin-bottom: 18px;"><?php the_title() ;?></h2>
+            <?php the_date('d/m/Y', '<time>', '</time>'); ?>
+            <?php the_content(); ?>
+    </div>
+    
+    <div class="fourcol last">
+        <?php get_template_part( 'sidebar', 'social'); ?>
+    </div>
 
 <?php get_footer(); ?>
