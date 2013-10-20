423800f422dbf393fe56ac702cc373a060e34a87
jefersonbraun <jefersonbraun@Jefersons-MacBook-Pro.local>
2013-10-19 21:05:57 -0300

    Post thumbnail funcionando na home

--- page-blog.php
+++ page-blog.php
@@ -12,17 +12,19 @@ get_header(); ?>
     
         <div class="loopPost">
             <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title() ;?></a></h2>
-            <?php the_date('d/m/Y', '<time class="postTime">', '</time>'); ?>
+            
             <br style="clear:both;" />
 			<br style="clear:both;" />
             <?php the_content( __( 'Leia mais <span class="meta-nav">&rarr;</span>' ) ); ?>
             <br style="clear:both;" />
 			<br style="clear:both;" />
             <p class="alignleft">
-            	<span class="italico">Escrito por</span> <span class="autor"><?php the_author(); ?></span>
+            	<span class="italico">Escrito por</span> <span class="dinpro"><?php the_author(); ?></span>
+            	<br style="clear:both;" />
+            	<?php the_date('d/m/Y', '<span class="italico">em</span> <time class="dinpro">', '</time>'); ?>
             </p>
-<a href="https://twitter.com/share" class="twitter-share-button alignright mleft10" data-url="<?php the_permalink(); ?>" data-text="" data-via="oclatil">Tweet</a>
-            <div class="fb-like alignright" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+            <div class="fb-like alignleft" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+            <a href="https://twitter.com/share" class="twitter-share-button alignleft mleft10" data-url="<?php the_permalink(); ?>" data-text="" data-via="oclatil">Tweet</a>
         </div>
         
         <?php endwhile; endif; ?>
@@ -33,7 +35,7 @@ get_header(); ?>
     </div>
     
     <div class="fourcol last">
-        <?php get_template_part( 'sidebar', 'social'); ?>
+        <!-- VAZIO -->
     </div>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -5,32 +5,34 @@
 		<div class="row paddingT36">
 			<div class="eightcol">
 				<h2 class='entry-title'><?php the_title() ;?></h2>
-				
-				<?php
-					if ( in_category('video') ) {
-						
-					} else {
-						the_date('d/m/Y', '<time class="postTime">', '</time>');
-					}
-				?>
-
+				<br style="clear:both;" />
+				<br style="clear:both;" />
+				<div class="fb-like alignleft" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+				<a href="https://twitter.com/share" class="twitter-share-button alignleft mleft10" data-url="<?php the_permalink(); ?>" data-text="" data-via="oclatil">Tweet</a>
 				<br style="clear:both;" />
 				<br style="clear:both;" />
 				<?php the_content(); ?>
-				<br style="clear:both;" />
-				<br style="clear:both;" />
 				<p class="alignleft">
-					<span class="italico">Escrito por</span> <span class="autor"><?php the_author(); ?></span>
+					<span class="italico">Escrito por</span> <span class="dinpro"><?php the_author(); ?></span>
+					<br style="clear:both;" />
+					<?php
+						if ( in_category('video') ) {
+							
+						} else {
+							the_date('d/m/Y', '<span class="italico">em</span> <time class="dinpro">', '</time>');
+						}
+					?>
 				</p>
-				<a href="https://twitter.com/share" class="twitter-share-button alignright mleft10" data-url="<?php the_permalink(); ?>" data-text="" data-via="oclatil">Tweet</a>
-				<div class="fb-like alignright" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+				<br style="clear:both;" />
+				<div class="fb-like alignleft" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-show-faces="false" data-send="false"></div>
+            <a href="https://twitter.com/share" class="twitter-share-button alignleft mleft10" data-url="<?php the_permalink(); ?>" data-text="" data-via="oclatil">Tweet</a>
 				<br style="clear:both;" />
 				<br style="clear:both;" />				
 				<div class="fb-comments" data-href="<?php the_permalink(); ?>" data-width="470"></div>
 			</div>
 			
 			<div class="fourcol last">
-			<?php get_template_part( 'sidebar', 'social'); ?>
+				<!-- vazio -->
 			</div>
 		</div>
 	
