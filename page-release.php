f32affcb99e85ff1a08881ef39106d93aedac507
jefersonbraun <jefersonbraun@Jefersons-MacBook-Pro.local>
2013-10-20 16:57:50 -0200

    alterações do Pedro antes do lançamento

--- page-release.php
+++ page-release.php
@@ -22,10 +22,10 @@ get_header(); ?>
     <div class="row">
         <div class="twocol"></div>
         <div class="fourcol marginT18">
-            <p>Coletivo musical formado em 2009 por Pedro DOM (multi instrumentista/compositor), Patrick Bass (baixo) e Zilla Sonoro (MC/beatmaker). Suas sonoridades sofisticadas não podem pertencer a um gênero único, pois quando há a mescla de ritmos brasileiros como o samba e a bossa nova, a erudição de Bethoven, Debussy e Villa Lobos sendo transmitidas pela energia viva e pulsante do Rap, o resultado é uma ecatombe para todos os amantes da música autêntica.</p>
+            <p align="justify">Coletivo musical formado em 2009 por Pedro DOM (multi instrumentista/compositor), Patrick Bass (baixo) e Zilla Sonoro (MC/beatmaker). Suas sonoridades sofisticadas não podem pertencer a um gênero único, pois quando há a mescla de ritmos brasileiros como o samba e a bossa nova, a erudição de Bethoven, Debussy e Villa Lobos sendo transmitidas pela energia viva e pulsante do Rap, o resultado é uma ecatombe para todos os amantes da música autêntica.</p>
         </div>
         <div class="fourcol marginT18">
-            <p>Em suas letras, o retrato de uma juventude pensante que busca melhorias sem radicalismos, usando a consciência e a voz em alerta a seus contemporâneos. Do erudito ao popular.</p>
+            <p align="justify">Em suas letras, o retrato de uma juventude pensante que busca melhorias sem radicalismos, usando a consciência e a voz em alerta a seus contemporâneos. Do erudito ao popular.</p>
             <br /><br />
             <address>Lucas Uilson (Zilla Sonoro)<br />MC/beatmaker na O.C.L.A.</address>
         </div>
